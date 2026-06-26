7ad731e604464721b0cc1cbe34cd904d6c403185
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-06-26 13:18:16 +0200

    feat: Test client now sends a series of messages after 1 second

--- app/src/main/java/com/miniredis/GreetingsClient.java
+++ app/src/main/java/com/miniredis/GreetingsClient.java
@@ -28,4 +28,35 @@ public class GreetingsClient {
         reader.close();
         clinetSocket.close();
     }
+
+
+
+    public static void main(String[] args) {
+
+        String[] msgs = {"Hello","How are you..?", "Can you hear us..?","is Anyone there..?"};
+        GreetingsClient client = new GreetingsClient();
+
+        try{
+            client.startConnection("127.0.0.0",6380);
+        }catch (Exception e){
+            return;
+        }
+
+        for (String msg: msgs){
+            
+            try{
+                System.out.println("Sending: " + msg);
+                client.sendMessage(msg);
+                String response = client.reader.readLine();
+                System.out.println("Response: " + response);
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                return;
+            }catch (IOException e){
+                return;
+            }catch (Exception e){
+                return;
+            }
+        }
+    }
 }
