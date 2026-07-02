2d4f4e6815653eaeb6d6de8ba74382ed01a410a7
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-02 15:42:12 +0200

    feat: created a Store class for the data handling to separate the data layer from network layer.

---
+++ app/src/main/java/com/miniredis/Store.java
@@ -0,0 +1,18 @@
+package com.miniredis;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Store {
+    private Map<String,String> data;
+    Store(){
+        data = new HashMap<>();
+    }
+    public void set(String key, String val){
+        data.put(key, val);
+    }
+
+    public String get(String key){
+        return data.getOrDefault(key,null);
+    }
+}
