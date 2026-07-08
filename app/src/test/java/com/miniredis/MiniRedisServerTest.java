d17de6c403fb50cf890683301f6c983e2ece7866
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-09 00:41:18 +0200

    Test: updated the types of args in servers test.

--- app/src/test/java/com/miniredis/MiniRedisServerTest.java
+++ app/src/test/java/com/miniredis/MiniRedisServerTest.java
@@ -2,20 +2,23 @@ package com.miniredis;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 public class MiniRedisServerTest {
     
     @Test
     void testHandleCommand() {
-        assertEquals("PONG", MiniRedisServer.handleCommand("PING", null));
+        assertEquals("PONG", MiniRedisServer.handleCommand(Arrays.asList("PING"), null));
     }
 
     @Test
     void testHandleGet() {
         Store store = new Store();
         store.set("Chauhan", "Suleman");
-        String[] msgs = {"GET","Chauhan"};
+        List<String> msgs = Arrays.asList("GET","Chauhan");
         assertEquals("Suleman", MiniRedisServer.handleGet(msgs, store));
     }
 
@@ -23,7 +26,7 @@ public class MiniRedisServerTest {
     void testHandleSet() {
         Store store = new Store();
         store.set("Chauhan", "Suleman");
-        String[] msgs = {"SET","Chauhan","Suleman"};
+        List<String> msgs = Arrays.asList("GET","Suleman","Chauhan");
         assertEquals("Ok.", MiniRedisServer.handleSet(msgs, store));
     }
 }
