6d1656797e1be85fdd10d050192408e3ebec9dd8
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-13 01:18:34 +0200

    feat: a constructor in RespWriter that takes an OutputStream.

--- app/src/main/java/com/miniredis/resp/RespWriter.java
+++ app/src/main/java/com/miniredis/resp/RespWriter.java
@@ -1,5 +1,13 @@
 package com.miniredis.resp;
 
+import java.io.OutputStream;
+
 public class RespWriter {
+    private final OutputStream output;
+
+    public RespWriter(OutputStream output){
+        this.output = output;
+    }
+
     
 }
