c782db5ed0cd80401a40ed1c4ff52f51c3b766f5
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-13 01:22:57 +0200

    feat: new method write in resp.RespWriter that takes resp.Response type args and write to output stream.

--- app/src/main/java/com/miniredis/resp/RespWriter.java
+++ app/src/main/java/com/miniredis/resp/RespWriter.java
@@ -1,6 +1,8 @@
 package com.miniredis.resp;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 public class RespWriter {
     private final OutputStream output;
@@ -9,5 +11,12 @@ public class RespWriter {
         this.output = output;
     }
 
-    
+    public void write(Response r){
+        try{
+            output.write(r.getResponse().getBytes(StandardCharsets.UTF_8));
+        
+        }catch (IOException e){
+            //TODO
+        }
+    }
 }
