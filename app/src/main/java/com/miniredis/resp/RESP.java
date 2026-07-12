51a9321ca325a3df8bf665619ecd4679d7740164
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-12 03:47:41 +0200

    feat: created new package resp for handling RESP related things

---
+++ app/src/main/java/com/miniredis/resp/RESP.java
@@ -0,0 +1,93 @@
+package com.miniredis.resp;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+public class RESP {
+    private DataInputStream in;
+
+    RESP(InputStream in){
+        this.in = new DataInputStream(in);
+    }
+
+
+    public List<String> readCommand() throws IOException{
+        List<String> result = new ArrayList<>();
+        int cur = in.read();
+        switch (cur) {
+            case '*':
+                result = handleArrays();
+                return result;
+            case '$':
+                result.add(handleBulkString());
+                return result;
+            default:
+                throw new IOException();
+        }
+    }
+
+    public List<String> handleArrays() throws IOException{
+        List<String> result = new ArrayList<>();
+        int total = Integer.parseInt(Character.toString(in.read()));
+        
+        int cr = in.read();
+        int lf = in.read();
+        if(cr != '\r' || lf != '\n'){
+            throw new IOException("ERR UnKnown Pattern.");
+        }
+
+        for(int i = 0; i<total; i++){
+            int cur = in.read();
+            switch (cur) {
+                case '*':
+                    List<String> r1 = handleArrays();
+                    result.addAll(r1);
+                    break;
+                case '$':
+                    result.add(handleBulkString());
+                    break;
+                default:
+                    throw new IOException();
+            } 
+        }
+        
+        // cr = in.read();
+        // lf = in.read();
+        // if(cr != '\r' || lf != '\n'){
+        //     throw new IOException("ERR UnKnown Pattern.");
+        // }
+
+        return result;    
+    }
+
+    public String handleBulkString() throws IOException{
+        StringBuilder sb = new StringBuilder();
+        int total = Integer.parseInt(Character.toString(in.read()));
+        
+        int cr = in.read();
+        int lf = in.read();
+        if(cr != '\r' || lf != '\n'){
+            throw new IOException("ERR UnKnown Pattern.");
+        }
+
+        for(int i = 0; i<total; i++){
+            int cur = in.read();
+            if (cur == -1){
+                throw new IOException();
+            }
+
+            sb.append(Character.toString(cur));
+        }
+
+        cr = in.read();
+        lf = in.read();
+        if(cr != '\r' || lf != '\n'){
+            throw new IOException("ERR UnKnown Pattern.");
+        }
+
+        return sb.toString();
+    }
+
+
+}
