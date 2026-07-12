65758b378568cd4c9ff2837e9e2d139a2849a3f5
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-13 01:31:12 +0200

    feat: added Respective fields in the clientHandler class

--- app/src/main/java/com/miniredis/server/ClientHandler.java
+++ app/src/main/java/com/miniredis/server/ClientHandler.java
@@ -2,10 +2,18 @@ package com.miniredis.server;
 
 import java.net.Socket;
 
+import com.miniredis.resp.RespReader;
+import com.miniredis.resp.RespWriter;
+
 public class ClientHandler {
+    private final Socket client;
+    private final RespReader reader;
+    private final RespWriter writer;
+    
+
     
     public void handleClient(Socket client){
         System.out.println("Server: Connected a new Client.");
-        
+
     }
 }
