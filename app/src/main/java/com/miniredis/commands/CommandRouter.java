28ce8cc25f794d8f3b3b7f719611c4fe197b1771
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-13 01:55:43 +0200

    feat: added respective methods in CommandRouter class.

--- app/src/main/java/com/miniredis/commands/CommandRouter.java
+++ app/src/main/java/com/miniredis/commands/CommandRouter.java
@@ -1,5 +1,7 @@
 package com.miniredis.commands;
 
+import java.util.List;
+
 import com.miniredis.data.Store;
 
 public class CommandRouter {
@@ -10,5 +12,48 @@ public class CommandRouter {
         this.store = store;
     }
 
-    
+    public String handleSet(List<String> cmds){
+        if(cmds.size() != 3){
+            return "Set Command only takes 2 arguments.";
+        }
+        String key = cmds.get(1);
+        String val = cmds.get(2);
+        if(key.length() == 0 || val.length() == 0){
+            return "Key or value cannot be empty.";
+        }
+
+        store.set(key, val);
+        return "Ok.";
+    }
+
+    public String handleGet(List<String> cmds){
+        if(cmds.size() != 2){
+            return "Get Command only takes 1 arguments.";
+        }
+        String key = cmds.get(1);
+        if(key.length() == 0){
+            return "Key cannot be empty.";
+        }
+        String val = store.get(key);
+        if(val == null){
+            return "No Value Found.";
+        }
+        return val;
+    }
+
+
+    public String handleCommand(List<String> cmds){
+
+        if(cmds.size() == 0){
+            return "Empty Command";
+        }
+
+        String cmd = cmds.getFirst().toUpperCase();
+        return switch(cmd){
+            case "SET" -> handleSet(cmds);
+            case "GET" -> handleGet(cmds);
+            case "PING" -> "PONG";
+            default     -> "ERR unknown Command '" + cmd + "'"; 
+        };
+    }
 }
