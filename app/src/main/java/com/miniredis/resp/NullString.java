374a52d39efd1e2a44d6aa39140678f3a3455297
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-12 04:10:39 +0200

    NullString: updated to Singleton pattern for robust design.

--- app/src/main/java/com/miniredis/resp/NullString.java
+++ app/src/main/java/com/miniredis/resp/NullString.java
@@ -2,7 +2,9 @@ package com.miniredis.resp;
 
 public class NullString extends Response{
     
-    public NullString(){
+    public static NullString instance = new NullString();
+    
+    private NullString(){
         this.response = "$-1\r\n";
     }
 
