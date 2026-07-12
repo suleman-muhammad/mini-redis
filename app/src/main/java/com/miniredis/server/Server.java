b2b58bf663944ba93c18e47f4646887f6416853b
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-13 01:03:29 +0200

    feat: created a new method start() in server.Server which starts the server at the port add and keep new accepting clients.

--- app/src/main/java/com/miniredis/server/Server.java
+++ app/src/main/java/com/miniredis/server/Server.java
@@ -1,16 +1,27 @@
 package com.miniredis.server;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
     
     private final int port;
-    private final ServerSocket server;
 
 
     public Server(int port){
         this.port = port;
-        this.server = null;
+    }
+
+    public void start(){
+        try(ServerSocket miniServer = new ServerSocket(this.port)){
+            System.out.println("Server: Started at Port " + this.port);
+            while(true){
+                Socket client = miniServer.accept();
+            }
+        }catch(IOException e){
+
+        }
     }
 
     
