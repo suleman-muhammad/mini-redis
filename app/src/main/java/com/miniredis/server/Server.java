427fbdddc47eac30f0ee3ae44056fe0e3a623952
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-13 00:41:51 +0200

    feat: new Server class with port and server as private fields. and a constructor that takes the port number as arg.

--- app/src/main/java/com/miniredis/server/Server.java
+++ app/src/main/java/com/miniredis/server/Server.java
@@ -1,5 +1,17 @@
 package com.miniredis.server;
 
+import java.net.ServerSocket;
+
 public class Server {
+    
+    private final int port;
+    private final ServerSocket server;
+
+
+    public Server(int port){
+        this.port = port;
+        this.server = null;
+    }
+
     
 }
