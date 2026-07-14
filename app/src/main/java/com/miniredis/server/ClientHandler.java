cf0abcdab10ecfa33ef0e91ee56e3692e507197d
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-14 17:48:31 +0200

    feat: integrated CommandRouter in the ClientHandler class.

--- app/src/main/java/com/miniredis/server/ClientHandler.java
+++ app/src/main/java/com/miniredis/server/ClientHandler.java
@@ -3,6 +3,7 @@ package com.miniredis.server;
 import java.io.IOException;
 import java.net.Socket;
 
+import com.miniredis.commands.CommandRouter;
 import com.miniredis.resp.RespReader;
 import com.miniredis.resp.RespWriter;
 
@@ -10,16 +11,17 @@ public class ClientHandler {
     private final Socket client;
     private final RespReader reader;
     private final RespWriter writer;
-    
+    private final CommandRouter cr;
 
-    public ClientHandler(Socket client) throws IOException{
+    public ClientHandler(Socket client,CommandRouter cr) throws IOException{
         this.client = client;
         this.reader = new RespReader(client.getInputStream());
         this.writer = new RespWriter(client.getOutputStream());
+        this.cr = cr;
     }
     
     public void handleClient(){
         System.out.println("Server: Connected a new Client.");
-
+        
     }
 }
