d818b5b251839114741febf4c52cb83b718c321f
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-14 18:17:00 +0200

    feat: handled the client with complete logging untill disconnected.

--- app/src/main/java/com/miniredis/server/ClientHandler.java
+++ app/src/main/java/com/miniredis/server/ClientHandler.java
@@ -1,11 +1,14 @@
 package com.miniredis.server;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.List;
 
 import com.miniredis.commands.CommandRouter;
 import com.miniredis.resp.RespReader;
 import com.miniredis.resp.RespWriter;
+import com.miniredis.resp.Response;
 
 public class ClientHandler {
     private final Socket client;
@@ -22,6 +25,16 @@ public class ClientHandler {
     
     public void handleClient(){
         System.out.println("Server: Connected a new Client.");
-        
+        try{
+            List<String> commands;
+            while((commands = reader.readCommand()) != null){
+                Response res = cr.handle(commands);
+                writer.write(res);
+            }
+            System.out.println("Server: Client Disconnected gracefully.");
+        }
+        catch (IOException e){
+            System.out.println("Server: Client Disconnected abruptly: " + e.getMessage());
+        }
     }
 }
