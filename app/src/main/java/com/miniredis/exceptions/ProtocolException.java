0a76b965e2c61d7aee9882a8fb81c083e2fe37cc
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-14 20:34:32 +0200

    update: make the ProtocolException constructor public.

--- app/src/main/java/com/miniredis/exceptions/ProtocolException.java
+++ app/src/main/java/com/miniredis/exceptions/ProtocolException.java
@@ -3,7 +3,7 @@ package com.miniredis.exceptions;
 import java.io.IOException;
 
 public class ProtocolException extends IOException{
-    ProtocolException(String message){
+    public ProtocolException(String message){
         super(message);
     }
 }
