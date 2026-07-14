ac0367a7b6d23b84d23e82d4af8711ef612fc6e5
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-14 18:07:37 +0200

    feat: integrated Response Type responses in the CommandRouter methods.

--- app/src/main/java/com/miniredis/commands/CommandRouter.java
+++ app/src/main/java/com/miniredis/commands/CommandRouter.java
@@ -3,6 +3,11 @@ package com.miniredis.commands;
 import java.util.List;
 
 import com.miniredis.data.Store;
+import com.miniredis.resp.BulkString;
+import com.miniredis.resp.ErrorString;
+import com.miniredis.resp.NullString;
+import com.miniredis.resp.Response;
+import com.miniredis.resp.SimpleString;
 
 public class CommandRouter {
     
@@ -12,48 +17,48 @@ public class CommandRouter {
         this.store = store;
     }
 
-    public String handleSet(List<String> cmds){
+    public Response handleSet(List<String> cmds){
         if(cmds.size() != 3){
-            return "Set Command only takes 2 arguments.";
+            return new ErrorString("Err Set Command only takes 2 arguments.");
         }
         String key = cmds.get(1);
         String val = cmds.get(2);
         if(key.length() == 0 || val.length() == 0){
-            return "Key or value cannot be empty.";
+            return new ErrorString("Err Key or value cannot be empty.");
         }
 
         store.set(key, val);
-        return "Ok.";
+        return new SimpleString("Ok.");
     }
 
-    public String handleGet(List<String> cmds){
+    public Response handleGet(List<String> cmds){
         if(cmds.size() != 2){
-            return "Get Command only takes 1 arguments.";
+            return new ErrorString("Err Get Command only takes 1 arguments.");
         }
         String key = cmds.get(1);
         if(key.length() == 0){
-            return "Key cannot be empty.";
+            return new ErrorString("Err Key cannot be empty.");
         }
         String val = store.get(key);
         if(val == null){
-            return "No Value Found.";
+            return NullString.instance;
         }
-        return val;
+        return new BulkString(val);
     }
 
 
-    public String handle(List<String> cmds){
+    public Response handle(List<String> cmds){
 
         if(cmds.size() == 0){
-            return "Empty Command";
+            return new ErrorString("ERR Empty Command");
         }
 
         String cmd = cmds.getFirst().toUpperCase();
         return switch(cmd){
             case "SET" -> handleSet(cmds);
             case "GET" -> handleGet(cmds);
-            case "PING" -> "PONG";
-            default     -> "ERR unknown Command '" + cmd + "'"; 
+            case "PING" -> new SimpleString("PONG");
+            default     -> new ErrorString("ERR unknown Command '" + cmd + "'"); 
         };
     }
 }
