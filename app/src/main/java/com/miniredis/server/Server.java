9bfd811399bea8c40153f72b9cf330ad6e129911
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-14 17:47:38 +0200

    feat: integrated CommandRouter in the server class.

--- app/src/main/java/com/miniredis/server/Server.java
+++ app/src/main/java/com/miniredis/server/Server.java
@@ -4,13 +4,16 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import com.miniredis.commands.CommandRouter;
+
 public class Server {
     
     private final int port;
+    private final CommandRouter cr;
 
-
-    public Server(int port){
+    public Server(int port,CommandRouter cr){
         this.port = port;
+        this.cr = cr;
     }
 
     public void start(){
@@ -18,13 +21,11 @@ public class Server {
             System.out.println("Server: Started at Port " + this.port);
             while(true){
                 Socket client = miniServer.accept();
-                ClientHandler handler = new ClientHandler(client); // handle IOException.
+                ClientHandler handler = new ClientHandler(client,cr); // handle IOException.
                 handler.handleClient();
             }
         }catch(IOException e){
 
         }
     }
-
-    
 }
