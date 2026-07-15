0525d602f9d3962b933156aa4f0fdf80dd5ae812
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-16 01:53:02 +0200

    feat: week 3 started, added executor service to server to handle multiple client at once.

--- app/src/main/java/com/miniredis/server/Server.java
+++ app/src/main/java/com/miniredis/server/Server.java
@@ -3,6 +3,8 @@ package com.miniredis.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import com.miniredis.commands.CommandRouter;
 
@@ -10,10 +12,12 @@ public class Server {
     
     private final int port;
     private final CommandRouter cr;
+    private final ExecutorService es;
 
     public Server(int port,CommandRouter cr){
         this.port = port;
         this.cr = cr;
+        es = Executors.newFixedThreadPool(50);
     }
 
     public void start(){
@@ -21,11 +25,18 @@ public class Server {
             System.out.println("Server: Started at Port " + this.port);
             while(true){
                 Socket client = miniServer.accept();
-                ClientHandler handler = new ClientHandler(client,cr); // handle IOException.
-                handler.handleClient();
+                try{
+                    ClientHandler handler = new ClientHandler(client,cr);
+                    es.submit(() -> {handler.handleClient();});
+                }catch (Exception e){
+
+                }
             }
         }catch(IOException e){
             System.out.println("Server: cannot Start the server.");
+        }finally{
+            es.shutdownNow();
+            System.out.println("Server: Shut down Complete.");
         }
     }
 }
