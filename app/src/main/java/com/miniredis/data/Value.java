50c41e00610110725d4c4343dc290e143f45796f
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-19 17:42:22 +0200

    feat: added infinite live key case check in the expiry checking method.

--- app/src/main/java/com/miniredis/data/Value.java
+++ app/src/main/java/com/miniredis/data/Value.java
@@ -3,6 +3,6 @@ package com.miniredis.data;
 public record Value(String val, long expiresAtMillis) {
     
     public boolean isExpired(){
-        return System.currentTimeMillis() >= expiresAtMillis;
+        return expiresAtMillis != 0 && System.currentTimeMillis() >= expiresAtMillis;
     }
 }
