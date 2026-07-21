8d244bc3d114b5ff2974f6e80010d841b466e58a
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-21 18:41:38 +0200

    update: removed try-catch cuse they are handled in their respective classes now.

--- app/src/main/java/com/miniredis/Main.java
+++ app/src/main/java/com/miniredis/Main.java
@@ -2,12 +2,17 @@ package com.miniredis;
 
 import com.miniredis.commands.CommandRouter;
 import com.miniredis.data.Store;
+import com.miniredis.persistence.AofWriter;
 import com.miniredis.server.Server;
 
 public class Main {
     public static void main(String[] args) {
         Store store = new Store();
+        
         CommandRouter router = new CommandRouter(store);
+        AofWriter.replay(router);
+        AofWriter aof = new AofWriter();
+        router.setAofWriter(aof);
         Server server = new Server(6380,router);
         server.start();
     }
