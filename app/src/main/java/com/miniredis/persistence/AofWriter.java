6470a30e9f50d2f6ff6f32f14ae8d86115cda69e
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-21 18:03:15 +0200

    feat: new reply static method to restore the previos state in the database.

--- app/src/main/java/com/miniredis/persistence/AofWriter.java
+++ app/src/main/java/com/miniredis/persistence/AofWriter.java
@@ -1,15 +1,21 @@
 package com.miniredis.persistence;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
+import com.miniredis.commands.CommandRouter;
+
 public class AofWriter {
-    private final FileWriter fw;
+    public static String FILE_PATH = ".\\commands.txt";
+    private FileWriter fw;
 
     public AofWriter(String file) throws IOException{
-        this.fw = new FileWriter(new File(".\\commands.txt"));
+        this.fw = new FileWriter(new File(FILE_PATH));
     }
 
     public synchronized void log(List<String> cmds) throws Exception{
@@ -33,6 +39,20 @@ public class AofWriter {
             System.out.println("Writer: Error in closing the Log file.");
             throw e;
         }
+    }
+
+    public static void replay(CommandRouter cr) throws Exception{
+        try(BufferedReader bf = new BufferedReader(new FileReader(FILE_PATH))){
+            String line;
+            while((line = bf.readLine()) != null){
+                List<String> cmds = Arrays.asList(line.split(" "));
+                cr.handle(cmds,true);
+            }
+        }catch(IOException e){
+            System.out.println("Writer: cannot Execute Reply.");
+            throw e;
+        }
+
     }
 
 }
