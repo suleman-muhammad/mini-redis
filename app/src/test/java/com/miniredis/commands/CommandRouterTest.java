866ee88a1a673c47df367c456de1d194ff2c9909
Suleman Muhammad <suleman.chauhan74@gmail.com>
2026-07-24 16:35:35 +0200

    feat: added test for normal Exists command.

--- app/src/test/java/com/miniredis/commands/CommandRouterTest.java
+++ app/src/test/java/com/miniredis/commands/CommandRouterTest.java
@@ -54,6 +54,14 @@ public class CommandRouterTest {
         assertEquals(":1\r\n", r.getResponse());
     }
 
+    @Test
+    void testExists(){
+        Response r = router.handle(List.of("SET","foo","bar"),false);
+        r = router.handle(List.of("EXISTS","foo"), false);
+        assertInstanceOf(RespInteger.class, r);
+        assertEquals(":1\r\n", r.getResponse());
+    }
+
 
 
 
